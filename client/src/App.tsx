adb93ce038c39c6366f609f4cc311e9fdc10d847
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2023-08-12 14:59:25 +0545

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='py-4 px-16 flex flex-col justify-between bg-slate-900 text-white min-h-screen'>
+      <p> Header </p>
+      <p> Main body </p>
+      <p> Footer </p>
+    </div>
+  )
+}
+
+export default App
